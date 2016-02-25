6b8f2fc69b5a55109acf0dc358a10d5b9a805c5e
Andrew Stroup <andrew.m.stroup@gmail.com>
2016-02-24 22:14:07 -0500

    added in js for side menu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
 $(document).ready(function() {
   
-  $('.usa-sidenav-list > a').on('click', function(){
-    console.log('test');
+  $('.usa-sidenav-list a').on('click', function(){
     $('.usa-sidenav-list a.usa-current').removeClass('usa-current');
     $(this).addClass('usa-current');
   });
 
+  $('#contracts, #certs, #ato, #testing').on('click', function() {
+    if ($('#' + $(this).attr('id') + '-list').css('display') == 'none'){
+      submenu_selector(this, 'show')
+    } else {
+      submenu_selector(this, 'hide')
+    };
+  });
+
 });
+
+function submenu_selector(id, setting){ 
+  if (setting == 'show') {
+    $('#' + $(id).attr('id') + '-list').css('display','inherit');
+    $(id).find('.fa').removeClass('fa-angle-down').addClass('fa-angle-up');
+  } else {
+    $('#' + $(id).attr('id') + '-list').css('display','none');
+    $(id).find('.fa').removeClass('fa-angle-up').addClass('fa-angle-down');
+  }
+}
