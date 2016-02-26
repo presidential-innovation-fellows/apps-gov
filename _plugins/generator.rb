7510f003146e8c52d564fa127aa0ce91c22351e9
Andrew Stroup <andrew.m.stroup@gmail.com>
2016-02-26 11:08:50 -0500

    switched to products

--- _plugins/generator.rb
+++ _plugins/generator.rb
@@ -3,23 +3,23 @@ require 'jekyll'
 module PIF
   module AppsGov
     class AppPage < ::Jekyll::Page
-      def initialize(site, app_info)
+      def initialize(site, product_info)
         @site = site
         @base = site.source
-        @dir = "apps/#{app_info['slug']}"
+        @dir = "products/#{product_info['slug']}"
         @name = 'index.html'
 
         process(name)
-        read_yaml(File.join(@base, '_layouts'), 'app.html')
-        @data = @data.merge('app_info' => app_info)
-        @data['title'] = app_info['name']
+        read_yaml(File.join(@base, '_layouts'), 'product.html')
+        @data = @data.merge('product_info' => product_info)
+        @data['title'] = product_info['name']
       end
     end
 
     class Generator < ::Jekyll::Generator
       def generate(site)
-        site.data['apps'].each do |app_slug, app_info|
-          site.pages << AppPage.new(site, app_info)
+        site.data['products'].each do |product_slug, product_info|
+          site.pages << AppPage.new(site, product_info)
         end
       end
     end
