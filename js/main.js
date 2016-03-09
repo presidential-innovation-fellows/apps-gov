3ccf794245a582a7646237c8e3ef1a9f88eca174
Andrew Stroup <andrew.m.stroup@gmail.com>
2016-03-09 13:37:11 -0500

    added isotope filtering and tags

--- js/main.js
+++ js/main.js
@@ -13,6 +13,81 @@ $(document).ready(function() {
     };
   });
 
+  /* Isotope stuff */
+  var $container = $('#product-list');
+
+  $container.imagesLoaded(function(){
+    $container.isotope({
+      itemSelector: '.product-list-item',
+      layoutMode: 'masonry'
+    });
+  });
+
+  var filters = {};
+
+  $('#filters').on( 'click', 'button', function() {
+    var $this = $(this);
+
+    // get group key
+    var $buttonGroup = $this.parents('.button-group');
+    var filterGroup = $buttonGroup.attr('data-filter-group');
+    // set filter for group
+    filters[ filterGroup ] = $this.attr('data-filter');
+    // combine filters
+    var filterValue = '';
+    for (var prop in filters) {
+      filterValue += filters[ prop ];
+    }
+    // set filter for Isotope
+    $container.isotope({ filter: filterValue });
+  });
+  
+  // change active class on buttons
+  $('#filter-service-type').each(function(i,buttonGroup){
+    var $buttonGroup = $(buttonGroup);
+    $buttonGroup.on('click', 'button', function(){
+     $buttonGroup.find('.active').removeClass('active').removeClass('btn-primary').addClass('btn-default');
+     $(this).addClass('active').removeClass('btn-default').addClass('btn-primary').blur();
+    })
+  })
+  $('#filter-category').each(function(i,buttonGroup){
+    var $buttonGroup = $(buttonGroup);
+    $buttonGroup.on('click', 'button', function(){
+     $buttonGroup.find('.active').removeClass('active').removeClass('btn-primary').addClass('btn-default');
+     $(this).addClass('active').removeClass('btn-default').addClass('btn-primary').blur();
+    })
+  })
+  $('#filter-certification').each(function(i,buttonGroup){
+    var $buttonGroup = $(buttonGroup);
+    $buttonGroup.on('click', 'button', function(){
+     $buttonGroup.find('.active').removeClass('active').removeClass('btn-primary').addClass('btn-default');
+     $(this).addClass('active').removeClass('btn-default').addClass('btn-primary').blur();
+    })
+  })
+
+
+  // isotope condition for tablet and phone
+  // $(window).resize(function(){
+  //   if ($(window).width() <= 991 ) {
+  //     $container.isotope({ filter: '' });
+
+  //     $('#region').each(function(i,buttonGroup){
+  //       var $buttonGroup = $(buttonGroup);
+  //       if ( $buttonGroup.find('.active').text() != 'All Regions' ) {
+  //         $(this).find('.active').removeClass('active').removeClass('btn-primary').addClass('btn-default');
+  //         $buttonGroup.children().first().addClass('active').removeClass('btn-default').addClass('btn-primary').blur();
+  //       }
+  //     });
+  //     $('#skill').each(function(i,buttonGroup){
+  //       var $buttonGroup = $(buttonGroup);
+  //       if ( $buttonGroup.find('.active').text() != 'All Skills' ) {
+  //         $(this).find('.active').removeClass('active').removeClass('btn-primary').addClass('btn-default');
+  //         $buttonGroup.children().first().addClass('active').removeClass('btn-default').addClass('btn-primary').blur();
+  //       }
+  //     });
+  //   }
+  // });
+
 });
 
 function submenu_selector(id, setting){ 
