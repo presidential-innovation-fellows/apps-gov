d827e0378cdd891821708219e053974b062300a2
Mike Bland <mbland@acm.org>
2016-03-10 10:38:24 -0500

    Sort matching products by search result score

--- js/products.js
+++ js/products.js
@@ -1,29 +1,41 @@
 'use strict';
 
-function getVisibleItemUrls(results) {
-  var visibleItemUrls = {},
+function getVisibleItemUrlsToScores(results) {
+  var visibleItemUrlsToScores = {},
       origin = location.origin || (location.protocol + '//' + location.host);
 
   results.forEach(function(result, index) {
-    visibleItemUrls[origin + result.url] = true;
+    visibleItemUrlsToScores[origin + result.url] = result.score;
   });
-  return visibleItemUrls;
+  return visibleItemUrlsToScores;
 }
 
-function filterMatchingItems(resultsList, visibleItemUrls) {
+function filterMatchingItems(resultsList, visibleItemUrlsToScores) {
   var item,
       i,
-      itemsToRemove = [];
+      score,
+      matchingItems = [];
 
-  for (i = 0; i != resultsList.children.length; ++i) {
+  for (i = 0; i !== resultsList.children.length; ++i) {
     item = resultsList.children[i];
-    if (visibleItemUrls[item.getElementsByTagName('a')[0].href] !== true) {
-      itemsToRemove.push(item);
+    score = visibleItemUrlsToScores[item.getElementsByTagName('a')[0].href];
+    if (score) {
+      item.score = score;
+      matchingItems.push(item);
     }
   };
+  matchingItems.sort(function(lhs, rhs) {
+    return rhs.score - lhs.score;
+  });
+  return matchingItems;
+}
 
-  itemsToRemove.forEach(function(item) {
-    resultsList.removeChild(item);
+function replaceListItemsWithMatchingItems(resultsList, matchingItems) {
+  while (resultsList.firstChild) {
+    resultsList.removeChild(resultsList.firstChild);
+  }
+  matchingItems.forEach(function(item) {
+    resultsList.appendChild(item);
   });
 }
 
@@ -31,7 +43,7 @@ function setTabIndices(resultsList) {
   var i,
       anchor;
 
-  for (i = 0; i != resultsList.children.length; ++i) {
+  for (i = 0; i !== resultsList.children.length; ++i) {
     anchor = resultsList.children[i].getElementsByTagName('a')[0];
     anchor.tabindex = i;
     if (i === 0) {
@@ -41,6 +53,9 @@ function setTabIndices(resultsList) {
 }
 
 module.exports = function(query, results, doc, resultsList) {
-  filterMatchingItems(resultsList, getVisibleItemUrls(results));
+  var matchingItems = filterMatchingItems(
+    resultsList, getVisibleItemUrlsToScores(results));
+
+  replaceListItemsWithMatchingItems(resultsList, matchingItems);
   setTabIndices(resultsList);
 }
