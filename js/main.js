edbe1d694a440c1dc6443cc34a2c3a9ae11456ef
Andrew Stroup <andrew.m.stroup@gmail.com>
2016-03-13 04:28:32 -0400

    update to listing page and config file

--- js/main.js
+++ js/main.js
@@ -13,6 +13,26 @@ $(document).ready(function() {
     };
   });
 
+  // The function actually applying the offset
+  function offsetAnchor() {
+      if(location.hash.length !== 0) {
+          window.scrollTo(window.scrollX, window.scrollY - 200);
+      }
+  }
+
+  // This will capture hash changes while on the page
+  $(window).on("hashchange", function () {
+      offsetAnchor();
+  });
+
+  // This is here so that when you enter the page with a hash,
+  // it can provide the offset in that case too. Having a timeout
+  // seems necessary to allow the browser to jump to the anchor first.
+  window.setTimeout(function() {
+      offsetAnchor();
+  }, 1); // The delay of 1 is arbitrary and may not always work right (although it did in my testing).
+
+
 });
 
 function submenu_selector(id, setting){ 
@@ -24,3 +44,4 @@ function submenu_selector(id, setting){
     $(id).find('.fa').removeClass('fa-angle-up').addClass('fa-angle-down');
   }
 }
+
